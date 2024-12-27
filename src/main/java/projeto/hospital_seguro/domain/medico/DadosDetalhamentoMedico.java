cf5b84476b95eea587c17dedc140c52fce0894a3
Rafael Caxixi <rafael.fuzeti@sptech.school>
2024-12-27 17:05:14 -0300

    Domain de medico/paciente/endereco finalizados

---
+++ src/main/java/projeto/hospital_seguro/domain/medico/DadosDetalhamentoMedico.java
@@ -0,0 +1,9 @@
+package projeto.hospital_seguro.domain.medico;
+
+public record DadosDetalhamentoMedico() {
+
+    public DadosDetalhamentoMedico(Medico medico){
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(), medico.getEndereco());
+    }
+
+}
